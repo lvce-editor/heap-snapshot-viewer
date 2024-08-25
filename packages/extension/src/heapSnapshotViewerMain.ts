cbd1cb51177bafa020af49bdd52e9746c7e657e8
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2024-08-25 20:16:47 +0200

    feature: experiment with webview / iframe api (#3)

--- packages/extension/src/heapSnapshotViewerMain.ts
+++ packages/extension/src/heapSnapshotViewerMain.ts
@@ -1,3 +1,16 @@
+const webViewProvider = {
+  id: 'builtin.heap-snapshot-viewer',
+  async open(uri, webView) {
+    // const content = await vscode.readFile(uri)
+    // webView.postMessage({
+    //   jsonrpc: '2.0',
+    //   method: 'setContent',
+    //   params: [content],
+    // })
+  },
+}
+
 export const activate = () => {
-  console.log('hello from heap snapshot viewer')
+  // @ts-ignore
+  vscode.registerWebViewProvider(webViewProvider)
 }
