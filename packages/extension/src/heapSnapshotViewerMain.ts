0b21b40e13e18163e001dce44f9f1a6b1ec9d5cb
Le Vivilet <levivlet@pm.me>
2024-08-29 19:04:18 +0200

    feature: try to display heapsnapshot instances

--- packages/extension/src/heapSnapshotViewerMain.ts
+++ packages/extension/src/heapSnapshotViewerMain.ts
@@ -1,3 +1,10 @@
+// TODO for best performance:
+// 1. create heapsnapshot worker
+// 2. create connection between heapsnapshot worker and iframe
+// 3. create connection between heapsnapshot worker and electron shared process
+// 4. read file, send file directly from shared process to heapsnapshot worker
+// 5. parse heapsnapshot file
+// 6. send visible regions from heapsnapshot worker to iframe
 const webViewProvider = {
   id: 'builtin.heap-snapshot-viewer',
   async create(webView, uri) {
