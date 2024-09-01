93234365dd02bffaaf6ddb77b25105fbdbec3788
Le Vivilet <levivlet@pm.me>
2024-09-01 13:03:10 +0200

    fix tsc

--- packages/extension/src/parts/HeapSnapshotWorkerUrl/HeapSnapshotWorkerUrl.ts
+++ packages/extension/src/parts/HeapSnapshotWorkerUrl/HeapSnapshotWorkerUrl.ts
@@ -1,3 +1,3 @@
-import * as AssetDir from '../AssetDir/AssetDir.js'
+import * as AssetDir from '../AssetDir/AssetDir.ts'
 
 export const heapSnapshotWorkerUrl = `${AssetDir.assetDir}/json-worker/src/jsonWorkerMain.ts`
