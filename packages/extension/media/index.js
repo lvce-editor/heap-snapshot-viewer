135a931369952325b94bc164fe6a63f53e573426
Le Vivilet <levivlet@pm.me>
2024-09-01 10:42:08 +0200

    feature: display heapsnapshot items as table

--- packages/extension/media/index.js
+++ packages/extension/media/index.js
@@ -1,11 +1,47 @@
 const setContent = (parsedNodes) => {
   // const overview = document.createElement('ul')
   // overview.className = 'Overview'
-  const pre = document.createElement('pre')
-  pre.className = 'HeapSnapshotView'
-  const stringified = JSON.stringify(parsedNodes, null, 2)
-  pre.textContent = stringified.slice(0, 1000)
-  document.body.append(pre)
+  const table = document.createElement('table')
+  table.className = 'Table'
+
+  const thead = document.createElement('thead')
+  thead.className = 'TableHeader'
+  const firstRow = document.createElement('tr')
+  firstRow.className = 'TableHeaderRow'
+  const firstColumnLabelCell = document.createElement('th')
+  firstColumnLabelCell.className = 'TableHeaderCell'
+  firstColumnLabelCell.textContent = 'Constructor'
+  firstRow.append(firstColumnLabelCell)
+  thead.append(firstRow)
+  table.append(thead)
+
+  const tbody = document.createElement('tbody')
+  tbody.className = 'TableBody'
+  for (const node of parsedNodes) {
+    const tr = document.createElement('tr')
+    tr.className = 'TableBodyRow'
+    const td1 = document.createElement('td')
+    td1.className = 'TableCell'
+
+    const classNameLabel = document.createElement('span')
+    classNameLabel.className = 'ClassName'
+    classNameLabel.textContent = node.name
+
+    const countLabel = document.createElement('span')
+    countLabel.className = 'CountLabel'
+    countLabel.textContent = `x ${node.count}`
+
+    td1.append(classNameLabel, countLabel)
+    tr.append(td1)
+    tbody.append(tr)
+  }
+  table.append(tbody)
+  document.body.append(table)
+  // const pre = document.createElement('pre')
+  // pre.className = 'HeapSnapshotView'
+  // const stringified = JSON.stringify(parsedNodes, null, 2)
+  // pre.textContent = stringified
+  // document.body.append(pre)
 }
 
 const rpc = globalThis.lvceRpc({
