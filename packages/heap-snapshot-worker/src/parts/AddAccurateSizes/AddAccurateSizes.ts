96ff37b96c527107d8023cf7a0e05f62a38efb1b
Le Vivilet <levivlet@pm.me>
2024-09-02 23:24:46 +0200

    add algorithm

--- packages/heap-snapshot-worker/src/parts/AddAccurateSizes/AddAccurateSizes.ts
+++ packages/heap-snapshot-worker/src/parts/AddAccurateSizes/AddAccurateSizes.ts
@@ -18,5 +18,48 @@ export const addAccurateSizes = (graph, nodes) => {
   while (worklist.length > 0) {
     const id = worklist.pop() as number
     const owner = owners[id]
+    const node = nodes[id]
+    const edges = graph[node.id] || []
+    for (const edge of edges) {
+      if (edge.type === 'weak') {
+        continue
+      }
+      const targetId = edge.nodeIndex
+      switch (owners[targetId]) {
+        case kUnvisited:
+          owners[targetId] = owner
+          worklist.push(targetId)
+          break
+        case targetId:
+        case owner:
+        case kHasMultipleOwners:
+          break
+        default:
+          owners[targetId] = kHasMultipleOwners
+          worklist.push(targetId)
+          break
+      }
+    }
+  }
+  for (let i = 0; i < nodes.length; i++) {
+    const ownerId = owners[i]
+    switch (ownerId) {
+      case kUnvisited:
+      case kHasMultipleOwners:
+      case i:
+        break
+      default:
+        const ownedNodeIndex = i
+        const ownerNodeIndex = ownerId
+        const owner = nodes[ownerNodeIndex]
+        if (owner.type === 'synthetic' || ownerNodeIndex === 0) {
+          break
+        }
+        const owned = nodes[ownedNodeIndex]
+        const sizeToTransfer = owned.size
+        owned.size = 0
+        owner.size += sizeToTransfer
+        break
+    }
   }
 }
