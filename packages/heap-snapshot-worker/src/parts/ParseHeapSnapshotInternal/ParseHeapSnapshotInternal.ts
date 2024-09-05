de5945e4f5b0c1b01d4615c08900f5713d753c95
Le Vivilet <levivlet@pm.me>
2024-09-05 23:02:16 +0200

    debug

--- packages/heap-snapshot-worker/src/parts/ParseHeapSnapshotInternal/ParseHeapSnapshotInternal.ts
+++ packages/heap-snapshot-worker/src/parts/ParseHeapSnapshotInternal/ParseHeapSnapshotInternal.ts
@@ -1,3 +1,4 @@
+import * as fs from 'node:fs'
 import * as AddAccurateSizes from '../AddAccurateSizes/AddAccurateSizes.ts'
 import * as NodeFieldType from '../NodeFieldType/NodeFieldType.ts'
 import * as ParseHeapSnapshotInternalEdges from '../ParseHeapSnapshotInternalEdges/ParseHeapSnapshotInternalEdges.ts'
@@ -13,6 +14,18 @@ export const parseHeapSnapshotInternal = (
   const nodeFieldCount = nodeFields.length
   const edgeCountOffset = nodeFields.indexOf(NodeFieldType.EdgeCount)
   const firstEdgeIndexes = ParseHeapSnapshotInternalEdges.parseHeapSnapshotInternalEdges(nodes, edgeCountOffset, nodeFieldCount)
+  const result: any[] = []
+  for (let i = 0; i < nodes.length; i += nodeFieldCount) {
+    const idOffset = 2
+    const edgeOffset = 4
+    const nodeId = nodes[i + idOffset]
+    const edgeCount = nodes[i + edgeOffset]
+    result.push({
+      nodeId,
+      edgeCount,
+    })
+  }
+  fs.writeFileSync('/tmp/b.json', JSON.stringify(result, null, 2) + '\n')
   AddAccurateSizes.addAccurateSizes(nodes, nodeFields, nodeTypes, edges, edgeFields, edgeTypes, firstEdgeIndexes)
   return {
     firstEdgeIndexes,
