4a90fddf9f796128db0a1bb4ed2e8a16140b1456
Le Vivilet <levivlet@pm.me>
2024-09-05 21:31:41 +0200

    improve parsing

--- packages/heap-snapshot-worker/src/parts/ParseHeapSnapshotInternalEdges/ParseHeapSnapshotInternalEdges.ts
+++ packages/heap-snapshot-worker/src/parts/ParseHeapSnapshotInternalEdges/ParseHeapSnapshotInternalEdges.ts
@@ -1,6 +1,6 @@
 export const parseHeapSnapshotInternalEdges = (nodes: Uint32Array, edgeCountOffset: number, nodeFieldCount: number) => {
   const nodeCount = nodes.length / nodeFieldCount
-  const firstEdgeIndexes = new Uint32Array(nodeCount + 1)
+  const firstEdgeIndexes = new Uint32Array(nodeCount)
   let edgeIndex = 0
   for (let i = 0; i < nodeCount; i++) {
     firstEdgeIndexes[i] = edgeIndex
--- packages/heap-snapshot-worker/test/ParseHeapSnapshotInternalEdges.test.ts
+++ packages/heap-snapshot-worker/test/ParseHeapSnapshotInternalEdges.test.ts
@@ -11,6 +11,6 @@ test('parseHeapSnapshotInternalEdges', () => {
   const edgeCountOffset = 3
   const nodeFieldCount = 7
   expect(ParseHeapSnapshotInternalEdges.parseHeapSnapshotInternalEdges(nodes, edgeCountOffset, nodeFieldCount)).toEqual(
-    new Uint32Array([0, 2, 0]),
+    new Uint32Array([0, 2]),
   )
 })
