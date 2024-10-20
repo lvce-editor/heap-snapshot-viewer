e4d53e1fa7380fc5c3501c5b674b0d1449e17c44
Le Vivilet <levivlet@pm.me>
2024-10-20 13:50:31 +0200

    tsc

--- packages/heap-snapshot-worker/src/parts/GetStatistics/GetStatistics.ts
+++ packages/heap-snapshot-worker/src/parts/GetStatistics/GetStatistics.ts
@@ -2,7 +2,7 @@ import * as HeapSnapshotState from '../HeapSnapshotState/HeapSnapshotState.ts'
 import * as GetStatisticsInternal from '../GetStatisticsInternal/GetStatisticsInternal.ts'
 
 export const getStatistics = (id: number) => {
-  const { nodes, nodeFields, nodeTypes, edges, edgeFields, edgeTypes, strings, firstEdgeIndexes } = HeapSnapshotState.get(id)
+  const { nodes, nodeFields, nodeTypes, strings } = HeapSnapshotState.get(id)
   const statistics = GetStatisticsInternal.getStatisicsInternal(nodes, nodeFields, nodeTypes, strings)
   return statistics
 }
