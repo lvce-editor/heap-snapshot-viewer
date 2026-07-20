2db4cd2a9c80a81d326a95bee4b0027ec4d4f280
Le Vivilet <levivilet@pm.me>
2026-07-20 13:21:36 +0200

    feature: simplify main (#114)

--- packages/extension/src/heapSnapshotViewerMain.ts
+++ packages/extension/src/heapSnapshotViewerMain.ts
@@ -1,5 +1,4 @@
 /* eslint-disable unicorn/no-top-level-side-effects */
-export { activate, deactivate } from './parts/Main/Main.ts'
 import { activate } from './parts/Main/Main.ts'
 
 await activate()
